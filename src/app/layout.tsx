801608da6c4c6db181f12408d14367cf42c2cd4b
Md limon shah <shahmdlimon372@gmail.com>
2026-05-22 19:21:29 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <meta name="google-adsense-account" content="ca-pub-2391987813376112">
         <Script
           id="adsense-script"
           async
